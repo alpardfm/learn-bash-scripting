e5b2ea307475588609e3edd336e1f1b971cdda04
alpardfm <tokferdian100@gmail.com>
2023-10-09 21:03:42 +0700

    add script for use variables

---
+++ variables/variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+name="Alpa"
+age=23
+timeNow=$(date)
+
+echo "Hello, my name is $name."
+echo "My age $age years old."
+echo "The system time and date is $timeNow"
+echo "My username is $USER"
